8743c6b7b7b5955afe1da3c0f15be99734e7a59f
AITechChannel <dta.bk13@gmail.com>
2022-06-02 15:50:55 +0700

    fix path and css wrapper tuananhDoan

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MainLayout from './components/layouts/MainLayout';
 
 const sessions = [
     { path: '/Practice-ReactJS/session1', tabName: 'Session1', component: Props },
-    { path: '/Practice-ReactJS', tabName: 'Session2', component: States },
+    { path: '/Practice-ReactJS/', tabName: 'Session2', component: States },
     { path: '/Practice-ReactJS/session3', tabName: 'Session3', component: States },
     { path: '/Practice-ReactJS/session4', tabName: 'Session4', component: States },
 ];
